8e13cacadd50b51d76fe9add63695df07bdf6d53
RuiInGitHub <rui.manuel.r.costa@gmail.com>
2024-09-21 20:55:45 +0100

    Commit inicial com .gitignore

--- Models/PesquisaViewModel.vb
+++
@@ -1,7 +0,0 @@
-﻿Namespace PortalDoCaneco.Models
-    Public Class PesquisaViewModel
-        Public Property ResultadosDinastias As List(Of Dinastia)
-        Public Property ResultadosMundo As List(Of Mundo)
-        Public Property Países As List(Of Dinastia)
-    End Class
-End Namespace
